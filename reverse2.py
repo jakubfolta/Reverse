28918e7de2ba554114bd670300392961ea949476
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-24 17:36:19 +0100

    continuation

--- reverse2.py
+++ reverse2.py
@@ -5,3 +5,12 @@ def print_reverse(text):
     return reverse_word
 
 print(print_reverse('fsdfrvwaaa'))
+
+
+def print_reverse(text):
+    rev_word = ''
+    for x in range(1, len(text) + 1):
+        rev_word += text[len(text) - x]
+    return rev_word
+
+print(print_reverse('abecadlo'))
