b547da782db0bac549c06b7f57595ace9d905622
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-05-07 18:20:21 +0100

    continue

--- reversehello.py
+++ reversehello.py
@@ -1,11 +1,14 @@
 def reverse(text):
     text = text.split('_')
     print(text)
-    n = 0
     new_word = ''
-    for x in text[n]:
-        print(x)
-        new_word
+    for item in text:
+        for x in range(1, len(item) + 1):
+            new_word += item[len(item) - x]
+        if text.index(item) == len(text) - 1:
+            break
+        new_word += '_'
+    return new_word
 
 
 print(reverse('hello_world'))
